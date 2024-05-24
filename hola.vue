ba0abe7923d0a00c13d030c603fb889065667763
georocha24 <georocha24@hotmail.es>
2024-05-23 18:24:43 -0600

    test

---
+++ hola.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Lorem Ipsun</p>
+        <div>
+            {{ text }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: "Hola"
+            }
+        },
+    }
+</script>
+
+<style>
+    p {
+        color: red;
+    }
+</style>
